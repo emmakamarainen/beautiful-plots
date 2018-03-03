646efd949d174ec69e802d27f01e1f06531c8435
Emma Kämäräinen <emma.kamarainen@dna.fi>
2018-03-03 11:50:34 +0200

    package list for reinstalling R

---
+++ installation/install_rpackages.R
@@ -0,0 +1,26 @@
+# R-packages
+
+pkg_list <- c(
+	'plyr',
+	'dplyr',
+	'data.table',
+	'lubridate',
+
+	'ggplot2',
+	'RColorBrewer',
+	'corrplot',
+
+	'RJDBC',
+	'rJava',
+	'rjson',
+	'RPostgres',
+	'iotools',
+
+	'glmnet',
+	'randomForest',
+	'ranger',
+	'xgboost',
+	'caret'
+)
+
+install.packages(p)
